1e0e54070abc2b29023b1bf25fa785fcbf66bd4f
kancers <kamba.ryosuke@lancers.co.jp>
2019-11-01 21:19:47 +0900

    feat: grpcサンプルアプリケーションの作成

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kancers/hello-grpc
+
+go 1.12
---
+++ server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/kancers/hello-grpc"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.Name)
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	addr := ":50051"
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	log.Printf("gRPC server listening on " + addr)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
